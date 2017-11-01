da3324831d15f15ad2957d9a7820b73eb812deb1
David Fekke <david@fekke.com>
2017-11-01 15:10:03 -0400

    Cleaning up mock

--- services/mockmeetupservice.js
+++ services/mockmeetupservice.js
@@ -4,13 +4,11 @@ const promisfy = require('util').promisify;
 const readFileAsync = promisfy(fs.readFile);
 
 async function getLocalTechEvents() {
-    const fEvents = await readFileAsync('./services/events.json');
-    return fEvents;
+    return await readFileAsync('./services/events.json');
 }
 
 async function getLocalGroups() {
-    const fGroups = await readFileAsync('./services/groups.json');
-    return fGroups;
+    return await readFileAsync('./services/groups.json');
 }
 
 exports.getLocalTechEvents = getLocalTechEvents;
