9d0224f18da02df56b399b8cfcd0bbf14de2853a
David Fekke <david@fekke.com>
2019-08-23 09:21:15 -0400

    Turning off most of the functionality :-(

--- routes/meetups.js
+++ routes/meetups.js
@@ -7,19 +7,7 @@ const formatTimes = meeting => {
 };
 
 async function events(req, res) {
-    try {
-        const events = await req.service.getLocalTechEvents();
-        const eventsGroupedByDate = events.reduce((acc, item) => {
-            const date = moment(item.time).format('dddd, MMMM Do YYYY');
-            acc[date] = acc[date] || [];
-            acc[date].push(item);
-            return acc;
-        }, {});
-        res.render('events', { title: 'Events', eventArray: eventsGroupedByDate });
-    } catch (err) {
-        throw err;
-    }
-    
+    res.render('home');
 }
 
 async function groups(req, res) {
